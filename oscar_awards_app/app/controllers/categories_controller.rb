2b4321eeeb57ec8527c1f689d0b694b3d416d3d9
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-04 21:19:33 -0400

    add query relationship to controller

--- oscar_awards_app/app/controllers/categories_controller.rb
+++ oscar_awards_app/app/controllers/categories_controller.rb
@@ -6,6 +6,24 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @nominations = @category.actors
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to @categories, notice: "Category sucessfully creted."}
+        format.json { render :show, status: :created, location: @category}
+      else
+        format.html { render :new}
+        format.json { render json: @categories, status: :unprocessable_entity}
+      end
+    end
   end
 
 end
