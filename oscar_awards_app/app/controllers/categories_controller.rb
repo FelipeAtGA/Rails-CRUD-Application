9303cac15959660b0fd52307c6c5b47a39988895
Felipe Hernandez <felipeneo@hotmail.com>
2017-10-04 22:21:24 -0400

    add create, edit, new, update, destroy methods to categories controller

--- oscar_awards_app/app/controllers/categories_controller.rb
+++ oscar_awards_app/app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
 
   def index
     @categories = Category.all
@@ -17,13 +18,43 @@ class CategoriesController < ApplicationController
     @category = Category.new(category_params)
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @categories, notice: "Category sucessfully creted."}
-        format.json { render :show, status: :created, location: @category}
+        format.html { redirect_to categories_path, notice: "Category sucessfully creted."}
+        format.json { render :show, status: :created, location: @categories}
       else
         format.html { render :new}
         format.json { render json: @categories, status: :unprocessable_entity}
       end
     end
   end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @category.update(category_params)
+        format.html { redirect_to @category, notice: 'Entry updated'}
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @category.destroy
+    respond_to do |format|
+      format.html { redirect_to categories_url, notice: 'Entry deleted'}
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
 
 end
